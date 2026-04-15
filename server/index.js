d8e9b49b9377f127bbca214c8375d3bd8b4b16ad
Dylan Panepucci <dylan.panepucci@gmail.com>
2026-04-14 21:41:05 -0400

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server: http://localhost:${PORT}`);
+});

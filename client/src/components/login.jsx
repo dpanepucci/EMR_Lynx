b98b14dbf87031c33015daa7dc3e8407c9321160
Dylan Panepucci <dylan.panepucci@gmail.com>
2026-04-18 23:24:17 -0400

    used svgr to link footer icons to github and linkedin at the bottom of the login page. CSS still needs to be finished to view properly

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,4 +1,6 @@
 import './login.css';
+import GithubIcon from '../assets/icons/github.svg?react';
+import LinkedIn from '../assets/icons/LinkedIn.svg?react';
 
 function Login () {
     return (
@@ -11,7 +13,7 @@ function Login () {
             </div>
             <div>
                 <label htmlFor="password">Password: </label>
-                <input type="text" id="password" name="password" required></input>
+                <input type="password" id="password" name="password" required></input>
             </div>
 
             <button type="submit" id="login">Login</button>
@@ -23,7 +25,18 @@ function Login () {
         <footer>
             <p>Created by: D.Panepucci</p>
             <div id="footer_icons">
-                
+                <a
+                href="https://github.com/dpanepucci"
+                target="_blank"
+                rel="noopener noreferrer">
+                <GithubIcon width={44} />
+                </a>
+                <a
+                href="https://linkedin.com/in/dylan-panepucci"
+                target="_blank"
+                rel="noopener noreferrer">
+                <LinkedIn width={44} />
+                </a>
             </div>
         </footer>
 

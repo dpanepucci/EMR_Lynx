4999f2479a213f2816587c75cdee0540dbc26e94
Dylan Panepucci <dylan.panepucci@gmail.com>
2026-05-17 20:53:40 -0400

    added hamburger icon for logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import './App.css'
+import Hamburger from './components/hamburgerMenu'
 
 function App() {
+
+  const [HamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburgerIcon = () =>{
+    setHamburgerOpen(!HamburgerOpen)
+  }
+
 
   return (
     <>
         <div>
           <h1>Healthcare Inc.</h1>
-          <p>
-          Axis Lynx - EMR
-          </p>
+          <div id='hamburgerNav'>
+            <p id='Axis'>Axis Lynx - EMR</p>
+          <ul className={HamburgerOpen ? 'menu open' : 'menu'}>
+            <li>Settings</li>
+            <li>Profile</li>
+            <li><a href='/login'>Log Out</a></li>
+          </ul>
+            <div className='navIcon' onClick={toggleHamburgerIcon}>
+              <Hamburger isOpen={HamburgerOpen} />
+            </div>
         </div>
-        <div id='pt_profile'>
+
+        </div>
+        {/* Side Patient Snap Shot */}
+        <div id='pt_profile'> 
           <img
           id='proPic'
             src='./defProPic.png'
---
+++ client/src/components/hamburgerMenu.jsx
@@ -0,0 +1,14 @@
+import './hamburgerMenu.css'
+
+export default function Hamburger({ isOpen }){
+    return(
+        <>
+            <div id='hamburgerMenu' className={isOpen ? 'open' : ''}>
+                <div className="burger"/>
+                <div className="burger"/>
+                <div className="burger"/>
+            </div>
+
+        </>
+    )
+}
